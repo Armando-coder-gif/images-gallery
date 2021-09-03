e51e7d9cdf41c4a56815399c5d8b189a7b23c52e
“Armando <armando.gonzalez@correo.unimet.edu.ve>
2021-09-03 15:02:35 -0400

    controlled search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,38 @@
+import  {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import Search from './components/Search';
 
 const App = () =>{
+  const [word, setWord] = useState('');
+
+
+  const handleSearchSubmit = (e) =>{
+    e.preventDefault();
+    console.log(word); // get the value written 
+  };
+
+
   return (
     <div>
       <Header title="Images Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
     </div>
   );
 }
 
 export default App;
+
+/*
+e = parameter
+Question to my brother:
+always I have to search the values with .values?
+e.target[0].value
+
+target elemento del dom
+como p, h1, div
+
+
+Note: Anytime when state of component changes, 
+component is re-rendered.
+*/
